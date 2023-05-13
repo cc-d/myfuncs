29844080ec7c17e7ca060ee562c3bd37d02fd7ce
Cary Carter <ccarterdev@gmail.com>
2023-05-13 14:15:27 -0500

    1.0.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='myfuncs',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[],
     author='Cary Carter',
     author_email='ccarterdev@gmail.com',
-    description='Personal utility functions for Cary Carter',
+    description='Personal utility functions that I use across different codebases.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/cc-d/myfuncs',
@@ -21,5 +21,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
